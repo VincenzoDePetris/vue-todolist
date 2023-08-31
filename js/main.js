b98634ae2360741794d62c6ad636b215a85ec0f6
VincenzoDePetris <vince02@live.it>
2023-08-31 14:13:23 +0200

    add: milestone 1

--- js/main.js
+++ js/main.js
@@ -2,6 +2,22 @@ const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      toDo: [
+        {
+          text: "fare la spesa",
+          done: true,
+        },
+        {
+          text: "fare allenamento",
+          done: false,
+        },
+        {
+          text: "portare a spasso il cane",
+          done: true,
+        },
+      ],
+    };
   },
+  methods: {},
 }).mount("#app");
